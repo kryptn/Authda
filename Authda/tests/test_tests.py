ccf70418a823bf2a2104a2905b2a1327afe4dd3c
kryptn <kryptn@gmail.com>
2017-01-10 18:34:12 -0700

    Initial code commit

---
+++ Authda/tests/test_tests.py
@@ -0,0 +1,7 @@
+import unittest
+
+class TestTestCase(unittest.TestCase):
+    def test_test(self):
+        self.assertTrue(True)
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Authda',
+      packages=['Authda'],
+      include_package_data=True,
+      test_suite='Authda.tests',
+      install_requires=['flask',
+                        'flask-sqlalchemy',
+                        'flask-testing',
+                        'psycopg2'])
+
